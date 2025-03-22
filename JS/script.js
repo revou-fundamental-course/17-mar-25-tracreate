a30f92db4059b6ee53565d3d88702799fa339a6a
Dewi <email@example.com>
2025-03-22 22:33:18 +0700

    third commit CSS dan JS Error

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,16 @@
 //# Ini JavaScript External
+//#javascrip masih salah semua
+function hitungKeliling() {
+    let sisi = document.getElementById("sisi").value;
+    if (sisi === "" || sisi <= 0) {
+        alert("Masukkan nilai sisi yang valid!");
+        return;
+    }
+    let keliling = 4 * sisi;
+    document.getElementById("hasil").innerText = keliling;
+}
+
+function resetForm() {
+    document.getElementById("sisi").value = "";
+    document.getElementById("hasil").innerText = "-";
+}
